ede9265cea266e4b2c164424d2fb82965990e690
PRIYANSUPULAK <priyansu.pulak30@gmail.com>
2019-06-27 17:56:06 +0530

    comments added

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import pandas as pd
 
+# Taking input from user
 s1 = raw_input("Enter input to test raw_input() function: ")
-print('\n')
+s1.lower()
+# Searching for buy keyword in the given query
 s2 = s1.find("buy")
 buying_query = 0
 selling_query = 0
+
+# If "buy" is found increase buying query to 1
 if s2!=-1:
 	global buying
 	buying_query = 1;
+# If "bought" is found increase buying query to 2
 s3 = s1.find("bought")
 if s3!=-1:
 	buying_query = 2;
+
+# Searching for "sell" keyword in the given question.
 s4 = s1.find("sell")
+
+# If "sell" is found, increase the selling_query to 1.
 if s4!=-1:
     selling_query = 1
+
+# Flag is used for checking. If flag == 1, its buying query. If flag == 2, its selling_queryself.
+# if Flag == 0. query is invalid
 flag = 0
 if selling_query == 1:
     flag = 2
