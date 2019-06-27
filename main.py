4f10ddf5cf138490d48746d1361e7129dd26d248
PRIYANSUPULAK <priyansu.pulak30@gmail.com>
2019-06-27 23:13:40 +0530

    queries can be taken now

--- main.py
+++ main.py
@@ -91,6 +91,60 @@ def home():
         json_obj=json.load(sock)
         htmlsource = sock.read()
         sock.close()
+        query = request.form["query"]
+        query = str(query)
+        query = query.lower()
+        words = []
+        words = query.split()
+        s2 = query.find("buy")
+        buying_query = 0
+        selling_query = 0
+
+        # If "buy" is found increase buying query to 1
+        if s2!=-1:
+        	buying_query = 1;
+        # If "bought" is found increase buying query to 2
+        s3 = query.find("bought")
+        if s3!=-1:
+        	buying_query = 2;
+
+        # Searching for "sell" keyword in the given question.
+        s4 = query.find("sell")
+
+        # If "sell" is found, increase the selling_query to 1.
+        if s4!=-1:
+            selling_query = 1
+
+        # Flag is used for checking. If flag == 1, its buying query. If flag == 2, its selling_queryself.
+        # if Flag == 0. query is invalid
+        flag = 0
+        if selling_query == 1:
+            flag = 2
+        elif buying_query>0:
+            flag=1
+        print("here")
+        if flag==2:
+        	print("sell")
+
+        elif flag==1:
+        	print("BUY")
+
+        else:
+            print("Invalid Query")
+
+        df = pd.read_csv("companylist.csv")
+        saved_column = df.Name
+        companies=[]
+        list=[]
+
+        for i in range(0,3482):
+        	z = saved_column[i].split()[0]
+        	z=z.split(',')[0]
+        	z=z.split('.')[0]
+        	list.append(z)
+
+        print(list[0])
+
         return render_template("results.html",json_obj=json_obj)
     else:
         return render_template("home.html")
